1ca0ac25b8b92dd0ddd0fac09330f47c669f723a
Mitchell-Carl <Mitchell.carl@student.northampton.edu>
2026-02-07 00:20:41 +0000

    Completed Payroll Assignment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  std::cout << "Payroll for Kathryn Janeway"<<endl;
+  const double BaseSalary = 1525.00;
+  const double TotalSales = 12253.78;
+  const double TotalExpenses = 195.75;
+  double TotalCommision, TotalPay;
+  TotalCommision = TotalSales*.032;
+  TotalPay = BaseSalary + TotalCommision + TotalExpenses;
+  cout << "Base Salary:" << setw(5) << BaseSalary << endl;
+  cout << "Commision:" << setw(10) << TotalCommision << endl;
+  cout << "Expenses:" << setw(10) << TotalExpenses << endl;
+  cout << setw(20) << "---------" << endl;
+  cout << "Total:" << setw(14) << TotalPay << endl;
 }
